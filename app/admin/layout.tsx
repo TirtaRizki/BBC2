c85f01159cc0d7104711a6c689ca74d34448717f
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-06 10:47:52 +0700

    galih: membuat sidebar untuk halaman dashboard admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 
 export default function AdminLayout({ 
   children 
@@ -10,6 +11,12 @@ export default function AdminLayout({
 }) {
   return (
     <div>
+         {/* Sidebar */}
+      <aside className="w-64 bg-white border-r">
+        <div className="p-4">
+          <h1 className="text-xl font-bold text-black">SecondBrand.co</h1>
+        </div>
+      </aside>
       {children}
     </div>
   );
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function AdminPage() {
   return (
     <div>
-      <h1 className='text-black'>Admin Dashboard</h1>
+        
     </div>
   );
 }
