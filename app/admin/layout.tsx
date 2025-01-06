e8c0dc61366387a8b3fd4a7ef835380362096d5d
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-06 10:29:12 +0700

    galih:membuat file baru untuk dahboard admin layout,page dan mengupload dokumentasi

---
+++ app/admin/layout.tsx
@@ -0,0 +1,16 @@
+// app/admin/layout.tsx
+'use client';
+
+import React from 'react';
+
+export default function AdminLayout({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1 className='text-black'>Admin Dashboard</h1>
+    </div>
+  );
+}
