fa83aaea561138bccd32ca8089c0d110b38f7b46
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-07 10:36:16 +0700

    galih: menginstall fontawesome dan membuat data dalam array matrics

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,14 @@
+import { faBox } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const DashboardMetrics = () => {
   const metrics = [
-
+    {
+      title: 'Total Products',  
+      value: '150',           
+      icon: <FontAwesomeIcon icon={faBox} className="w-6 h-6 text-blue-500" />,
+    },
   ];
 
   return(
