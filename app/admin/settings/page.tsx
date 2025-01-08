c19a009cc97917e9b4d105de6ce4ef6f096dcbf3
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-01-08 16:50:37 +0700

    nurhidayat: menambahkan elemen dasar dan header untuk page settings

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,7 +1,10 @@
 export default function SettingsPage() {
     return (
-        <div>
-            <h1>Settings</h1>
+        <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">Settings</h1>
+        <div className="bg-white p-6 rounded shadow-md">
+            <h2 className="text-xl font-bold mb-4">Store Settings</h2>
         </div>
+    </div>
     );
 }
