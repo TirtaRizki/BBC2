910bf3c1a15292739af5bd20fd3bb901f9091769
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-09 13:06:03 +0700

    Galih: Mengembalikan judul yang terhapus

--- app/admin/testimonials/page.tsx
+++ app/admin/testimonials/page.tsx
@@ -6,6 +6,7 @@ import React from "react"
 const TestimonialPage = () => {
     return (
         <main>
+            <h1 className="text-2xl font-bold mb-4 text-black">Testimonials Management</h1>
             <div className="bg-white rounded-lg shadow-sm p-6">
                 <div className="flex justify-between mb-4">
                     <div className="flex items-center gap-3">
