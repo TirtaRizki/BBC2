c0da9c83c1a5bb873dacbe74dc6b841b4ed48879
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-09 10:53:57 +0700

    Galih: Membuat icon pencarian pada halaman testimonials

--- app/admin/testimonials/page.tsx
+++ app/admin/testimonials/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Search } from "lucide-react";
 import React from "react"
 
 const TestimonialPage = () => {
@@ -8,8 +9,20 @@ const TestimonialPage = () => {
             <div className="max-w-7xl mx-auto">
                 <h1 className="text-3xl font-bold mb-8 text-black">Testimonials</h1>
             </div>
+            {/* Search */}
+            <div className="mb-6">
+                <div className="relative max-w-md">
+                    <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+                    <input
+                        type="text"
+                        placeholder="Search testimonials..."
+                        className="pl-10 pr-4 py-2 w-full rounded-lg border-0 ring-1 ring-gray-200"
+                    />
+                </div>
+            </div>
         </main>
     )
 };
 
 export default TestimonialPage;
+
