1d8c2d0c683181be1b489a67b5e033a24272701a
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-01-09 08:13:54 +0700

    nurhidayat: memanggil state pada semua fields input di folder settings

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -19,31 +19,43 @@ export default function SettingsPage() {
             <h1 className="text-2xl font-bold mb-4 text-black">Settings</h1>
             <div className="bg-white p-6 rounded shadow-md">
                 <h2 className="text-xl font-bold mb-4 text-black">Store Settings</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
+                    {/* input store name */}
                     <div className="mb-4">
                         <label className="block text-sm font-bold mb-2 text-black">Store Name</label>
                         <input
                             type="text"
+                            value={storeName}
+                            onChange={(e) => setStoreName(e.target.value)}
                             className="w-full p-2 border rounded bg-gray-800 text-white"
                         />
                     </div>
+                    {/* Input Contact Email */}
                     <div className="mb-4">
                         <label className="block text-sm font-bold mb-2 text-black">Contact Email</label>
                         <input
                             type="email"
+                            value={contactEmail}
+                            onChange={(e) => setContactEmail(e.target.value)}
                             className="w-full p-2 border rounded bg-gray-800 text-white"
                         />
                     </div>
+                    {/* input phone number */}
                     <div className="mb-4">
                         <label className="block text-sm font-bold mb-2 text-black">Phone Number</label>
                         <input
                             type="text"
+                            value={phoneNumber}
+                            onChange={(e) => setPhoneNumber(e.target.value)}
                             className="w-full p-2 border rounded bg-gray-800 text-white"
                         />
                     </div>
+                    {/* input addres */}
                     <div className="mb-4">
                         <label className="block text-sm font-bold mb-2 text-black">Address</label>
                         <textarea
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
                             className="w-full p-2 border rounded bg-gray-800 text-white"
                             rows={4}
                         ></textarea>
