b533c5cf7a3081131dc0b56586aba86ecd40fee8
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-01-13 12:48:29 +0700

    nurhidayat: Menambahkan Features Section pada page categories

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -17,7 +17,37 @@ const Categories = () => {
       </div>
 
       {/* Features Section */}
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
+        <div>
+          <div>
+            {/* Icon */}
+          </div>
+          <div>
+            <h3>100% Authentic</h3>
+            <p>Guaranteed original brands</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            {/* Icon */}
+          </div>
+          <div>
+            <h3>Secure Payment</h3>
+            <p>Multiple payment options</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            {/* Icon */}
+          </div>
+          <div>
+            <h3>Fast Delivery</h3>
+            <p>Nationwide shipping</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
