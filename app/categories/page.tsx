badd1a00753a1e460e48f615306a3eaeb80f445c
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-01-13 12:13:41 +0700

    nurhidayat: Melakukan setup awal pada page categories

---
+++ app/categories/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div>
+      {/* Header Section */}
+      <div></div>
+
+      {/* Buttons Section */}
+      <div></div>
+
+      {/* Features Section */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Categories;
