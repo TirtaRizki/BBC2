20356ef852a73d62776f9fe3f9e6001874d83a0d
TirtaRizki <tirtarzk30@gmail.com>
2025-01-20 01:51:16 +0700

    TirtaRizki: membuat button login di halman login admin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -92,9 +92,15 @@ return (
               Forgot password?
             </a>
             </div>
-
-
-
+                  {/* Submit Button */}
+                  <div>
+            <button
+              type="submit"
+              className="w-full py-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            >
+              Login
+            </button>
+          </div>
         </form>
       </div>
     </LoginLayout>
