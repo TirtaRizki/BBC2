f220d109f73a1f6d8bf9bf074a43f03873b26627
TirtaRizki <tirtarzk30@gmail.com>
2025-01-23 03:32:45 +0700

    TirtaRizki: Membuat halaman kosong testimonials user

---
+++ app/testimonials/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import { Star, Clock } from 'lucide-react';
+
+const TestimonialPage = () => {
+  const testimonials = [
+    {
+        
+    },
+
+  ];
+
+
+return (
+    <div className="min-h-screen bg-gray-50 pt-16">
+        
+
+
+    <div className="max-w-7xl mx-auto px-4 py-16">
+
+
+        </div>
+    </div>
+  );
+};
+
+export default TestimonialPage;
