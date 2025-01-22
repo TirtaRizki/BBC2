65455cd169e7c9a67827cb98d429cf0fb86a0804
Nurh1dayatt <nurhidayat@teknokrat.ac.id>
2025-01-22 08:44:28 +0700

    nurhidayat: Setup awal About Page

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Award, Users, ShoppingBag, Heart } from 'lucide-react';
+
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 pt-16">
+    </div>
+  );
+};
+
+export default AboutPage;
