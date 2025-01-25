ac70a94c4bdbf5577c9d7c1928e907f855623a67
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-25 07:48:35 +0700

    Galih: Memerbaiki kesalahan penulisan kata 'previoud' ke 'previous

--- app/admin/testimonials/page.tsx
+++ app/admin/testimonials/page.tsx
@@ -208,7 +208,7 @@ const TestimonialPage = () => {
                     <p className="text-gray-500">Showing 1 of 10 of 100 entries </p>
                     <div className="flex gap-2">
                         <button className="px-4 py-2 border rounded text-black">First</button>
-                        <button className="px-4 py-2 border rounded text-black">Previoud</button>
+                        <button className="px-4 py-2 border rounded text-black">Previous</button>
                         <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">1</button>
                         <button className="px-4 py-2 border rounded text-gray-600 hover:bg-gray-50">2</button>
                         <button className="px-4 py-2 border rounded text-gray-600 hover:bg-gray-50">3</button>
