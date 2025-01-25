8dcbb76d2aa533a75133374b4dfacc885627f87c
GalihPribadiUtomo <galihutomo.75@gmail.com>
2025-01-25 07:16:21 +0700

    Galih: Merubah background pada teks "Berikutnya"

--- app/admin/testimonials/page.tsx
+++ app/admin/testimonials/page.tsx
@@ -205,10 +205,10 @@ const TestimonialPage = () => {
                         </select>
                         <span className="text-gray-500">entries</span>
                     </div>
-                    <p className="text-gray-500">Showing 1 of 10 of 100 </p>
+                    <p className="text-gray-500">Showing 1 of 10 of 100 entries </p>
                     <div className="flex gap-2">
                         <button className="px-4 py-2 border rounded text-black">Sebelumnya</button>
-                        <button className="px-4 py-2 bg-blue-600 text-white rounded">Berikutnya</button>
+                        <button className="px-4 py-2 border rounded text-black">Berikutnya</button>
                         <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">1</button>
                     </div>
                 </div>
